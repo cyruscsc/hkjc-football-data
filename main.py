90854ea3dab0b6a707faaa50ca0ba4d3ac2949f9
Cyrus Chan <111417390+cyruscsc@users.noreply.github.com>
2022-09-23 18:22:20 +0800

    Add new function and refactor codes
    Big Changes:
    A new function of getting upcoming matches data is added. This program can now scrap data of both past and ongoing/upcoming football matches.
    
    Minor Changes: 
    More detailed progress statements. 
    Codes refactored to increase readability and modularity.

--- main.py
+++ main.py
@@ -6,36 +6,41 @@ from dateutil.relativedelta import relativedelta
 from requests.adapters import HTTPAdapter, Retry
 
 
-URL = "https://bet.hkjc.com/football/getJSON.aspx"
+def talk_to_hkjc(params: dict):
+    """Communicate with HKJC's website to get data."""
+    url = "https://bet.hkjc.com/football/getJSON.aspx"
+    session = requests.Session()
+    retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])  # dont get error please
+    session.mount("https://", HTTPAdapter(max_retries=retries))
+    response = session.get(url=url)
+    cookies = dict(response.cookies)
+    response = session.post(url=url, params=params, cookies=cookies)
+    session.close()
+    return response.json()
 
 
-def get_data():
-    """Get match data including results and last odds as JSON from HKJC."""
+def get_past_matches():
+    """Get results and last odds of finished matches."""
 
-    def get_results() -> list:
-        """Get match results data as JSON from HKJC."""
+    def get_match_ids() -> list:
+        """Get match results of finished matches."""
         end_date = datetime.today()
         start_date = end_date - relativedelta(months=1)
         # get total number of matches first
-        session = requests.Session()
-        retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])  # dont get error please
-        session.mount("https://", HTTPAdapter(max_retries=retries))
-        response = session.get(url=URL)
-        cookies = dict(response.cookies)
         params = {
             "jsontype": "search_result.aspx",
             "startdate": start_date.strftime("%Y%m%d"),
             "enddate": end_date.strftime("%Y%m%d"),
             "pageno": 1
         }
-        response = session.post(url=URL, params=params, cookies=cookies)
-        results = response.json()
+        results = talk_to_hkjc(params=params)
         matches_count = int(results[0]["matchescount"])
         pages_count = math.ceil(matches_count/20)
         # really going to get all match results
-        results_list = []
-        print(f"total matches: {matches_count}")
-        print(f"total pages: {pages_count}")
+        match_ids = []
+        print(f"Total matches: {matches_count}")
+        print(f"Total pages: {pages_count}")
+        print(">> Start getting match IDs...")
         for page_no in range(1, pages_count + 1):
             params = {
                 "jsontype": "search_result.aspx",
@@ -43,99 +48,130 @@ def get_data():
                 "enddate": end_date.strftime("%Y%m%d"),
                 "pageno": page_no
             }
-            response = session.post(url=URL, params=params, cookies=cookies)
-            results_list.extend(response.json()[0]["matches"])
-            print(f"page {page_no} done.")
-            if page_no % 10 == 0:
-                # pretend to be a human regularly
-                session.close()
-                session = requests.Session()
-                session.mount("https://", HTTPAdapter(max_retries=retries))
-            else:
-                pass
-        session.close()
-        return results_list
+            results = talk_to_hkjc(params=params)
+            for match in results[0]["matches"]:
+                match_ids.append(match["matchID"])
+            print(f">>>> Page {page_no} done.")
+        print(">> All match IDs retrieved.")
+        return match_ids
 
-    def get_last_odds(results_list: list) -> list:
-        """Get last odds data of a selected match as JSON from HKJC."""
-        ids = [match["matchID"] for match in results_list]
+    def get_last_odds(match_ids: list) -> list:
+        """Get last odds of finished matches."""
         last_odds_list = []
-        session = requests.Session()
-        retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])  # dont get error please
-        session.mount("https://", HTTPAdapter(max_retries=retries))
-        response = session.get(url=URL)
-        cookies = dict(response.cookies)
         n = 0
-        for id in ids:
+        print(">> Start getting last odds of matches...")
+        for match_id in match_ids:
             n += 1
             params = {
                 "jsontype": "last_odds.aspx",
-                "matchid": id
+                "matchid": match_id
             }
-            response = session.post(url=URL, params=params, cookies=cookies)
-            print(f"match {n} - match id: {id} - {response}")
-            last_odds = response.json()
+            last_odds = talk_to_hkjc(params=params)
             last_odds_list.append(last_odds)
-            if n % 50 == 0:
-                # pretend to be a human regularly
-                session.close()
-                session = requests.Session()
-                session.mount("https://", HTTPAdapter(max_retries=retries))
-            else:
-                pass
-        session.close()
+            print(f">>>> match {n} - match id: {match_id} - done")
+        print(">> All last odds retrieved.")
         return last_odds_list
 
-    match_results_list = get_results()
-    match_last_odds_list = get_last_odds(results_list=match_results_list)
-    match_data = {
-        "Match ID": [match["matchID"] for match in match_results_list],
-        "Match Date": [match["matchDate"] for match in match_results_list],
-        "Tournament": [match["tournament"]["tournamentNameEN"] for match in match_results_list],
-        "Home Team": [match["homeTeam"]["teamNameEN"] for match in match_results_list],
-        "Away Team": [match["awayTeam"]["teamNameEN"] for match in match_results_list],
-        "Half-time Scores": [f'{match_last_odds["accumulatedscore"][0]["home"]}-{match_last_odds["accumulatedscore"][0]["away"]}' if "accumulatedscore" in match_last_odds and len(match_last_odds["accumulatedscore"]) >= 2 else "None" for match_last_odds in match_last_odds_list],
-        "Half-time Home Odds": [match_last_odds["fhaodds"]["H"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Half-time Draw Odds": [match_last_odds["fhaodds"]["D"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Half-time Away Odds": [match_last_odds["fhaodds"]["A"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Full-time Scores": [f'{match_last_odds["accumulatedscore"][1]["home"]}-{match_last_odds["accumulatedscore"][1]["away"]}' if "accumulatedscore" in match_last_odds and len(match_last_odds["accumulatedscore"]) >= 2 else "None" for match_last_odds in match_last_odds_list],
-        "Full-time Home Odds": [match_last_odds["hadodds"]["H"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Full-time Draw Odds": [match_last_odds["hadodds"]["D"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Full-time Away Odds": [match_last_odds["hadodds"]["A"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Handicap Home": [match_last_odds["hdcodds"]["HG"] if "hdcodds" in match_last_odds else "None" for match_last_odds in match_last_odds_list],
-        "Handicap Away": [match_last_odds["hdcodds"]["AG"] if "hdcodds" in match_last_odds else "None" for match_last_odds in match_last_odds_list],
-        "Handicap Home Odds": [match_last_odds["hdcodds"]["H"].replace("100@", "").replace("000@", "") if "hdcodds" in match_last_odds else "None" for match_last_odds in match_last_odds_list],
-        "Handicap Away Odds": [match_last_odds["hdcodds"]["A"].replace("100@", "").replace("000@", "") if "hdcodds" in match_last_odds else "None" for match_last_odds in match_last_odds_list],
-        "Handicap HAD Home": [match_last_odds["hhaodds"]["HG"] for match_last_odds in match_last_odds_list],
-        "Handicap HAD Away": [match_last_odds["hhaodds"]["AG"] for match_last_odds in match_last_odds_list],
-        "Handicap HAD Home Odds": [match_last_odds["hhaodds"]["H"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Handicap HAD Draw Odds": [match_last_odds["hhaodds"]["D"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Handicap HAD Away Odds": [match_last_odds["hhaodds"]["A"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Odd Number Scores Odds": [match_last_odds["ooeodds"]["O"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "Even Number Scores Odds": [match_last_odds["ooeodds"]["E"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "First Team to Score Home Odds": [match_last_odds["ftsodds"]["H"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "First Team to Score None Odds": [match_last_odds["ftsodds"]["N"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list],
-        "First Team to Score Away Odds": [match_last_odds["ftsodds"]["A"].replace("100@", "").replace("000@", "") for match_last_odds in match_last_odds_list]
+    return get_last_odds(match_ids=get_match_ids())
+
+
+def get_upcoming_matches():
+    """Get details and all odds of unfinished matches."""
+
+    def get_match_ids() -> list:
+        """Get match ids of unfinished matches."""
+        params = {
+            "jsontype": "fullmatchlist"
+        }
+        print(">> Start getting match IDs...")
+        results = talk_to_hkjc(params=params)
+        match_ids = [match["mID"] for match in results]
+        print(">> All match IDs retrieved.")
+        return match_ids
+
+    def get_all_odds(match_ids: list) -> list:
+        """Get all odds of unfinished matches."""
+        all_odds_list = []
+        n = 0
+        print(">> Start getting all odds of matches...")
+        for match_id in match_ids:
+            n += 1
+            params = {
+                "jsontype": "odds_allodds.aspx",
+                "matchid": match_id
+            }
+            results = talk_to_hkjc(params=params)
+            for match in results["matches"]:
+                if match["matchID"] == match_id:
+                    all_odds_list.append(match)
+                else:
+                    pass
+            print(f">>>> match {n} - match id: {match_id} - done")
+        print(">> All odds retrieved.")
+        return all_odds_list
+
+    return get_all_odds(match_ids=get_match_ids())
+
+
+def process_data(function) -> dict:
+    """Turn the data received into a dictionary which can be used to build Pandas dataframe."""
+    original_data = function()
+    print(">> Start processing data...")
+    processed_data = {
+        "Match ID": [match["matchID"] for match in original_data],
+        "Match Date": [match["matchDate"] for match in original_data],
+        "Tournament": [match["tournament"]["tournamentNameEN"] for match in original_data],
+        "Home Team": [match["homeTeam"]["teamNameEN"] for match in original_data],
+        "Away Team": [match["awayTeam"]["teamNameEN"] for match in original_data],
+        "Half-time Scores": [f'{match["accumulatedscore"][0]["home"]}-{match["accumulatedscore"][0]["away"]}' if "accumulatedscore" in match and len(match["accumulatedscore"]) >= 2 else "None" for match in original_data],
+        "Half-time Home Odds": [match["fhaodds"]["H"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Half-time Draw Odds": [match["fhaodds"]["D"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Half-time Away Odds": [match["fhaodds"]["A"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Full-time Scores": [f'{match["accumulatedscore"][1]["home"]}-{match["accumulatedscore"][1]["away"]}' if "accumulatedscore" in match and len(match["accumulatedscore"]) >= 2 else "None" for match in original_data],
+        "Full-time Home Odds": [match["hadodds"]["H"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Full-time Draw Odds": [match["hadodds"]["D"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Full-time Away Odds": [match["hadodds"]["A"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Handicap Home": [match["hdcodds"]["HG"] if "hdcodds" in match else "None" for match in original_data],
+        "Handicap Away": [match["hdcodds"]["AG"] if "hdcodds" in match else "None" for match in original_data],
+        "Handicap Home Odds": [match["hdcodds"]["H"].replace("100@", "").replace("000@", "") if "hdcodds" in match else "None" for match in original_data],
+        "Handicap Away Odds": [match["hdcodds"]["A"].replace("100@", "").replace("000@", "") if "hdcodds" in match else "None" for match in original_data],
+        "Handicap HAD Home": [match["hhaodds"]["HG"] for match in original_data],
+        "Handicap HAD Away": [match["hhaodds"]["AG"] for match in original_data],
+        "Handicap HAD Home Odds": [match["hhaodds"]["H"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Handicap HAD Draw Odds": [match["hhaodds"]["D"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Handicap HAD Away Odds": [match["hhaodds"]["A"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Odd Number Scores Odds": [match["ooeodds"]["O"].replace("100@", "").replace("000@", "") for match in original_data],
+        "Even Number Scores Odds": [match["ooeodds"]["E"].replace("100@", "").replace("000@", "") for match in original_data],
+        "First Team to Score Home Odds": [match["ftsodds"]["H"].replace("100@", "").replace("000@", "") for match in original_data],
+        "First Team to Score None Odds": [match["ftsodds"]["N"].replace("100@", "").replace("000@", "") for match in original_data],
+        "First Team to Score Away Odds": [match["ftsodds"]["A"].replace("100@", "").replace("000@", "") for match in original_data]
     }
-    return match_data
+    print(">> All data processed.")
+    return processed_data
 
 
-def make_sheet(data: dict):
+def generate_sheet(data: dict, file_name: str):
     """Create new or update existing .csv file with provided data."""
     # create or append csv file with dataframe
+    print(">> Start generating CSV file...")
     try:
-        with open(file="hkjc_football.csv", mode="r"):
+        with open(file=file_name, mode="r"):
             pass
     except FileNotFoundError:
         df = pandas.DataFrame.from_dict(data=data, orient="columns").set_index("Match ID")
-        df.to_csv(path_or_buf="hkjc_football.csv", mode="w")
+        df.to_csv(path_or_buf=file_name, mode="w")
     else:
         df = pandas.DataFrame.from_dict(data=data, orient="columns").set_index("Match ID")
-        df.to_csv(path_or_buf="hkjc_football.csv", mode="a", header=False)
+        df.to_csv(path_or_buf=file_name, mode="a", header=False)
     # remove duplicated data in csv file
-    df = pandas.read_csv(filepath_or_buffer="hkjc_football.csv").set_index("Match ID")
+    df = pandas.read_csv(filepath_or_buffer=file_name).set_index("Match ID")
     df.drop_duplicates(inplace=True)
-    df.to_csv(path_or_buf="hkjc_football.csv", mode="w")
+    df.to_csv(path_or_buf=file_name, mode="w")
+    print(">> CSV file saved.")
 
 
-make_sheet(get_data())
+print("Start getting data of past matches...")
+generate_sheet(data=process_data(function=get_past_matches), file_name="hkjc_football_past.csv")
+print("Start getting data of upcoming matches...")
+generate_sheet(data=process_data(function=get_upcoming_matches), file_name="hkjc_football_upcoming.csv")
+print("Finished.")
